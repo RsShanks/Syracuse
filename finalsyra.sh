7951bfc2a7c2ea56e42507dc248ca972e94b1215
Anass Seddiki <anass.seddiki@1plusx.com>
2022-02-11 01:02:03 +0100

    Factorisation du script shell

--- finalsyra.sh
+++ finalsyra.sh
@@ -3,70 +3,70 @@
 
 var1=$1
 var2=$2
-mkdir UndeN
-mkdir temporaire
-while [ $var1 -le $var2 ]
-do
-    ./syracuse $var1 f$var1.dat
-    mv f$var1.dat UndeN
-    echo "`grep -v -e"n" -e"=" UndeN/f$var1.dat`" > temporaire/f$var1.dat # ca récupère tous les n pour chaque Un d'un U0
-    echo "$var1 `tail -3 UndeN/f$var1.dat | head -n+1 | cut -f 2 -d ' '`"  >> dureevol.dat  #récup la durée de vol
-    echo "$var1 `tail -2 UndeN/f$var1.dat | head -n+1 | cut -f 2 -d ' '`" >> altimax.dat  #recupere l'altidute max
-    echo "$var1 `tail -1 UndeN/f$var1.dat | cut -f 2 -d ' '`" >> dureeAlti.dat #recupere durée altix
-    let  "var1+=1"
-done
+
+create_directories () {
+    mkdir -pv UndeN
+    mkdir -pv temporaire
+    mkdir -pv données_syracuse
+    mkdir -pv graphique_syracuse
+}
+
+move_data_and_graphs () {
+    mv dureeAlti.dat altimax.dat dureevol.dat données_syracuse/
+    mv dureealti.jpeg dureevol.jpeg grapheAltitudeMax.jpeg TousLesUn.jpeg graphique_syracuse/
+
+}
+
+run_syracuse () {
+    U0=$1
+
+    ./syracuse $U0 f$U0.dat
+
+    mv f$U0.dat UndeN
+
+    echo "`grep -v -e"n" -e"=" UndeN/f$U0.dat`" > ./temporaire/f$U0.dat # ca récupère tous les n pour chaque Un d'un U0
+    echo "$U0 `tail -3 UndeN/f$U0.dat | head -n+1 | cut -f 2 -d ' '`"  >> ./dureevol.dat  #récup la durée de vol
+    echo "$U0 `tail -2 UndeN/f$U0.dat | head -n+1 | cut -f 2 -d ' '`" >> ./altimax.dat  #recupere l'altidute max
+    echo "$U0 `tail -1 UndeN/f$U0.dat | cut -f 2 -d ' '`" >> ./dureeAlti.dat #recupere durée altix
+}
+
+
+create_plot () {
+    echo "Création du graphique : " $1
+    title=$1
+    xlabel=$2
+    ylabel=$3
+    file_name=$4
+    plot_data=$5
+    plot_range=$6
 
     gnuplot << EOF
     set terminal jpeg size 800,600
-    set output 'TousLesUn.jpeg'
+    set output '$file_name'
     unset key
-    set title "Graphique de tous les Un"
-    set xlabel 'n'
-    set ylabel 'Un'
-    plot for[c=$1:$2]'temporaire/f'.c.'.dat' w l lt 1 lc 22
+    set title "$title"
+    set xlabel '$xlabel'
+    set ylabel '$ylabel'
+    $plot_range
+    plot $plot_data w l lt 1 lc 22
     quit
 EOF
+}
 
 
-gnuplot << EOF
-set terminal jpeg size 800,600
-set output 'grapheAltitudeMax.jpeg'
-unset key
-set title "Graphe AltitudeMax"
-set xlabel 'n'
-set ylabel 'Un'
-set xrange [$1:$2]
-plot "altimax.dat" w l lt 1 lc 22
-quit
-EOF
+create_directories
+
+while [ $var1 -le $var2 ]
+do
+    run_syracuse $var1
+    let  "var1+=1"
+done
+
+create_plot 'Graphique de tous les Un' n Un TousLesUn.jpeg "for[c=$1:$2]'temporaire/f'.c.'.dat'"
+create_plot 'Graphe AltitudeMax' n Un grapheAltitudeMax.jpeg '"altimax.dat"' "set xrange [$1:$2]"
+create_plot 'Durée de vol' n Un dureevol.jpeg '"dureevol.dat"' "set xrange [$1:$2]"
+create_plot 'Duree altitude' n Un dureealti.jpeg '"dureeAlti.dat"' "set xrange [$1:$2]"
 
 
-gnuplot << EOF
-set terminal jpeg size 800,600
-set output 'dureevol.jpeg'
-unset key
-set title "durée de vol"
-set xlabel 'n'
-set ylabel 'Un'
-set xrange [$1:$2]
-plot "dureevol.dat" w l lt 1 lc 22
-quit
-EOF
-
-
-gnuplot << EOF
-set terminal jpeg size 800,600
-set output 'dureealti.jpeg'
-unset key
-set title "duree altitude"
-set xlabel 'n'
-set ylabel 'Un'
-set xrange [$1:$2]
-plot "dureeAlti.dat" w l lt 1 lc 22
-quit
-EOF
-mkdir -p données_syracuse
-mv dureeAlti.dat altimax.dat dureevol.dat données_syracuse/
-            mkdir -p graphique_syracuse
-mv dureealti.jpeg dureevol.jpeg grapheAltitudeMax.jpeg TousLesUn.jpeg graphique_syracuse/
+move_data_and_graphs
 rm -r temporaire
